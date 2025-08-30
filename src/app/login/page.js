25abc40c8e449506ee4d9f59ce20ffcb9ae37b65
kazamijimin <mendozanicknarry@gmail.com>
2025-08-30 10:18:50 +0800

    login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -300,10 +300,7 @@ export default function LoginPage() {
   return (
     <>
       {/* Import Poppins Font */}
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet"
-      />
+    
 
       <div style={currentStyles.container}>
         {/* Dark/Light Mode Toggle */}
